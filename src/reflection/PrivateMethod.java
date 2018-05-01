9cb0fbc63cf2afa48b3abd9b6a045a287845d2a9
vinkrish <vinaykrishna1989@gmail.com>
2018-05-01 16:03:15 +0530

    reflection concepts added

---
+++ src/reflection/PrivateMethod.java
@@ -0,0 +1,7 @@
+package reflection;
+
+public class PrivateMethod {
+	private void message() {
+		System.out.println("hello java");
+	}
+}
---
+++ src/reflection/PrivateMethodTest.java
@@ -0,0 +1,15 @@
+package reflection;
+
+import java.lang.reflect.Method;
+
+public class PrivateMethodTest {
+
+	public static void main(String[] args) throws Exception {
+		Class c = Class.forName("reflection.PrivateMethod");  
+	    Object o = c.newInstance();
+	    Method m = c.getDeclaredMethod("message", null);  
+	    m.setAccessible(true);  
+	    m.invoke(o, null);  
+	}
+
+}
