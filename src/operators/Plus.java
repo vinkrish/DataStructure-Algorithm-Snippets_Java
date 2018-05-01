129c7e47fc57f509093a0f172cff0d25b60e998a
vinkrish <vinaykrishna1989@gmail.com>
2018-05-01 08:48:57 +0530

    Postfix implementation and evaluation added

--- src/operators/Plus.java
+++ src/operators/Plus.java
@@ -3,6 +3,6 @@ package operators;
 public class Plus {
 	public static void main(String args[])  {
        System.out.println(10  +  20 + "GeeksQuiz"); 
-       System.out.println("GeeksQuiz" + 10 + 20); 
+       System.out.println("GeeksQuiz" + 10 + 20);
    } 
 }
