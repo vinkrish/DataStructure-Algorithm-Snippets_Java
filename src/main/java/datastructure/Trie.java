595653211c07cba892919f8160c5a3cdb6e5ea3b
Vinay Krishna <vinaykrishna1989@gmail.com>
2021-09-21 19:30:39 -0400

    added remove to trie

--- src/main/java/datastructure/Trie.java
+++ src/main/java/datastructure/Trie.java
@@ -9,8 +9,7 @@ public class Trie {
 	static final int ALPHABET_SIZE = 26;
 	
 	// trie node
-	static class TrieNode
-	{
+	static class TrieNode {
 		TrieNode[] children = new TrieNode[ALPHABET_SIZE];
 	
 		// isEndOfWord is true if the node represents
@@ -26,20 +25,16 @@ public class Trie {
 	
 	static TrieNode root;
 	
-	// If not present, inserts key into trie
-	// If the key is prefix of trie node,
-	// just marks leaf node
-	static void insert(String key)
-	{
-		int level;
-		int length = key.length();
-		int index;
-	
+	/*
+	 * If not present, inserts key into trie.
+	 * If the key is prefix of trie node, just marks leaf node.
+	 */
+	static void insert(String key) {
 		TrieNode pCrawl = root;
 	
-		for (level = 0; level < length; level++)
+		for (int level = 0; level < key.length(); level++)
 		{
-			index = key.charAt(level) - 'a';
+			int index = key.charAt(level) - 'a';
 			if (pCrawl.children[index] == null)
 				pCrawl.children[index] = new TrieNode();
 	
@@ -51,23 +46,63 @@ public class Trie {
 	}
 	
 	// Returns true if key presents in trie, else false
-	static boolean search(String key)
-	{
-		int level;
-		int length = key.length();
-		int index;
+	static boolean search(String key) {
 		TrieNode pCrawl = root;
 	
-		for (level = 0; level < length; level++) {
-			index = key.charAt(level) - 'a';
+		for (int level = 0; level < key.length(); level++) {
+			int index = key.charAt(level) - 'a';
 			pCrawl = pCrawl.children[index];
 			if (pCrawl == null)  return false;
 		}
 		return (pCrawl.isEndOfWord);
 	}
 	
-	public static void main(String args[])
-	{
+	// Returns true if root has no children, else false
+    static boolean isEmpty(TrieNode root) {
+        for (int i = 0; i < ALPHABET_SIZE; i++)
+            if (root.children[i] != null)
+                return false;
+        return true;
+    }
+    
+    // Recursive function to delete a key from given Trie
+    static TrieNode remove(TrieNode root, String key, int depth) {
+        // If tree is empty
+        if (root == null)
+            return null;
+ 
+        // If last character of key is being processed
+        if (depth == key.length()) {
+ 
+            // This node is no more end of word after
+            // removal of given key
+            if (root.isEndOfWord)
+                root.isEndOfWord = false;
+ 
+            // If given is not prefix of any other word
+            if (isEmpty(root)) {
+                root = null;
+            }
+ 
+            return root;
+        }
+ 
+        // If not last character, recur for the child
+        // obtained using ASCII value
+        int index = key.charAt(depth) - 'a';
+        root.children[index] =
+            remove(root.children[index], key, depth + 1);
+ 
+        // If root does not have any child (its only child got
+        // deleted), and it is not end of another word.
+        if (isEmpty(root) && root.isEndOfWord == false){
+            root = null;
+        }
+ 
+        return root;
+    }
+	
+	public static void main(String args[]) {
 		// Input keys (use only 'a' through 'z' and lower case)
 		String keys[] = {"the", "a", "there", "answer", "any",
 						"by", "bye", "their"};
@@ -83,21 +118,24 @@ public class Trie {
 			insert(keys[i]);
 	
 		// Search for different keys
-		if(search("the") == true)
+		if(search("the"))
 			System.out.println("the --- " + output[1]);
 		else System.out.println("the --- " + output[0]);
 		
-		if(search("these") == true)
+		if(search("these"))
 			System.out.println("these --- " + output[1]);
 		else System.out.println("these --- " + output[0]);
 		
-		if(search("their") == true)
+		if(search("their"))
 			System.out.println("their --- " + output[1]);
 		else System.out.println("their --- " + output[0]);
 		
-		if(search("thaw") == true)
-			System.out.println("thaw --- " + output[1]);
-		else System.out.println("thaw --- " + output[0]);
+		System.out.println("Removing 'their'");
+		remove(root, "their", 0);
+		
+		if(search("their"))
+			System.out.println("their --- " + output[1]);
+		else System.out.println("their --- " + output[0]);
 		
 	}
 }
