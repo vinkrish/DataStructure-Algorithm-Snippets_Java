d8694752b19e003311c7975c81108c4e505ce625
Vinay Krishna <vinkrish@Vinays-MacBook-Air.local>
2022-02-13 23:38:05 -0500

    moved to vs code

--- src/main/java/reification/AbstractList.java
+++ src/main/java/reification/AbstractList.java
@@ -28,73 +28,61 @@ public class AbstractList<E> extends AbstractCollection<E> implements List<E> {
 
 	@Override
 	public boolean addAll(int index, Collection<? extends E> c) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public E set(int index, E element) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void add(int index, E element) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public E remove(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
